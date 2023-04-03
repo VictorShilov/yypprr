8eacb00ff1d349970963fabe60159dfb375dab62
Emil <longisland_band@mail.ru>
2023-04-03 16:36:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    Button button;
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editTextNumber);
+
+        button = (Button) findViewById(R.id.button);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                if (!text.equalsIgnoreCase("")) {
+                    int seconds = Integer.valueOf(editText.getText().toString());
+                    CountDownTimer countDownTimer = new CountDownTimer(seconds * 1000, 1000) {
+                        @Override
+                        public void onTick(long millis) {
+                            textView.setText("seconds: " + (int) (millis / 1000));
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            textView.setText("Тут должен быть звук, но да ладно)");
+                        }
+                    }.start();
+                }
+            }
+        });
+    }
+}
